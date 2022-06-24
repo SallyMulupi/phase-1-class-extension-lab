3384be2af5d5b4564f2cc99ea19e1f5fa5268fac
SallyMulupi <mulupisally2gmail.com>
2022-06-24 22:34:12 +0300

    get perimeter

--- index.js
+++ index.js
@@ -5,3 +5,10 @@ constructor(sides); {
 get = countSides(); {
     return this.sides.length;
 }
+get perimeter() {
+    let sum = 0;
+    for (let i = 0; i < this.sides.length; i++) {
+        sum += this.sides[i];
+    }
+    return sum;
+}
